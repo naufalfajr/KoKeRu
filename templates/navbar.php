8a76326074e64e8b42ab3e0273ba1197674e7c71
rizkybaihaqy <rizkybaihaqy123@gmail.com>
2020-12-05 19:45:36 +0700

    menambahkan fitur login untuk cs

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,3 +1,10 @@
+<?php
+session_start(); //insisalisasi session
+if (!isset($_SESSION['nama'])) {
+    header('Location: login.php');
+}
+$nama = $_SESSION['nama'];
+?>
 <!------------------------------------------------------------ NAVBAR ------------------------------------------------------------>
 <header class="mdl-layout__header">
     <div class="mdl-layout__header-row">
@@ -16,12 +23,13 @@
             <div class="pls-margin-x mdl-layout--large-screen-only">
                 <button id="account" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
                     <span class="material-icons">account_circle</span>
-                    <span class="account-name">Doni Hermawan</span>
+                    <span class="account-name"><?php echo $nama; ?></span>
                 </button>
                 <!-- list menu saat tombol di click -->
                 <ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect" for="account">
                     <!-- menu yang hanya digunakan pada fase development -->
                     <li class="mdl-menu__item"><a href="manager/">Ganti Akun (dev only)</a></li>
+                    <li class="mdl-menu__item"><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
         </nav>
